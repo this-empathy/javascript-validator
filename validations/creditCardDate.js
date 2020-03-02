301f4c75f3acac31f97c704f5bdb877e6f72574f
Giovanni Spolidorio <spolidorio@gmail.com>
2020-03-02 13:59:42 -0300

    add validation with date year with two digits

--- validations/creditCardDate.js
+++ validations/creditCardDate.js
@@ -7,8 +7,12 @@ export default (name, value) => {
   const res = value.split('/')
   const cardDate = new Date()
   let today = new Date()
-  cardDate.setFullYear(res[1], res[0], 1)
+  const yLength = res[1].length
 
+  if (yLength > 2 && yLength < 4 ) return field
+  let yy = yLength == 4 ? res[1] : `20${res[1]}`
+  
+  cardDate.setFullYear(yy, res[0], 1)
   field.valid = cardDate > today 
   
   return field
