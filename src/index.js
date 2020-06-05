645b90693a6d93bbb2bb5218545d418a6edc319e
Giovanni Sabino <giosab@intra.olos.com.br>
2020-06-05 16:20:08 -0300

    add es2015 support

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import carPlate from '../validations/carPlate';
+import cep from '../validations/cep';
+import cnh from '../validations/cnh';
+import cnpj from '../validations/cnpj';
+import cpf from '../validations/cpf';
+import cpfCnpj from '../validations/cpfCnpj';
+import date from '../validations/date';
+import email from '../validations/email';
+import fileSize from '../validations/fileSize';
+import fileType from '../validations/fileType';
+import fileTypeCsv from '../validations/fileTypeCsv';
+import letter from '../validations/letter';
+import number from '../validations/number';
+import phone from '../validations/phone';
+import regex from '../validations/regex';
+import renavam from '../validations/renavam';
+import required from '../validations/required';
+import age from '../validations/age';
+import creditCard from '../validations/creditCard';
+import creditCardDate from '../validations/creditCardDate';
+import creditCardNumber from '../validations/creditCardNumber';
+import creditCardType from '../validations/creditCardType';
+import rg from '../validations/rg';
+import specialCharacters from '../validations/specialCharacters';
+
+export default { 
+  carPlate,
+  cep,
+  cnh,
+  cnpj,
+  cpf,
+  cpfCnpj,
+  date,
+  email,
+  fileSize,
+  fileType,
+  fileTypeCsv,
+  letter,
+  number,
+  phone,
+  regex,
+  renavam,
+  required,
+  age,
+  creditCard,
+  creditCardDate,
+  creditCardNumber,
+  creditCardType,
+  rg,
+  specialCharacters
+}
--- tests/validations/cpfCnpj.spec.js
+++ tests/validations/cpfCnpj.spec.js
@@ -1,4 +1,4 @@
-import validator from '../../index.js'
+import validator from '../../src/index.js'
 
 describe('CPF&CNPJ', () => {
   test('Validates CPF&CNPJ', () => {
